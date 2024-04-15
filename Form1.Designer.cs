92a77e5615d3578878a9a2deb8620fbf2f9c75f9
EvanWhiteRoy <Evanwhit225@ed.amdsb.ca>
2024-04-15 12:33:35 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Robot_Adventure_Final
+{
+    partial class Robot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.optionButton1 = new System.Windows.Forms.Button();
+            this.optionButton2 = new System.Windows.Forms.Button();
+            this.optionButton3 = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // optionButton1
+            // 
+            this.optionButton1.Location = new System.Drawing.Point(200, 260);
+            this.optionButton1.Name = "optionButton1";
+            this.optionButton1.Size = new System.Drawing.Size(75, 23);
+            this.optionButton1.TabIndex = 0;
+            this.optionButton1.UseVisualStyleBackColor = true;
+            this.optionButton1.Click += new System.EventHandler(this.optionButton1_Click);
+            // 
+            // optionButton2
+            // 
+            this.optionButton2.Location = new System.Drawing.Point(12, 260);
+            this.optionButton2.Name = "optionButton2";
+            this.optionButton2.Size = new System.Drawing.Size(75, 23);
+            this.optionButton2.TabIndex = 1;
+            this.optionButton2.UseVisualStyleBackColor = true;
+            this.optionButton2.Click += new System.EventHandler(this.optionButton2_Click);
+            // 
+            // optionButton3
+            // 
+            this.optionButton3.Location = new System.Drawing.Point(108, 260);
+            this.optionButton3.Name = "optionButton3";
+            this.optionButton3.Size = new System.Drawing.Size(75, 23);
+            this.optionButton3.TabIndex = 2;
+            this.optionButton3.UseVisualStyleBackColor = true;
+            this.optionButton3.Click += new System.EventHandler(this.optionButton3_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(123, 160);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(12, 28);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(325, 84);
+            this.titleLabel.TabIndex = 5;
+            this.titleLabel.Text = "Welcome to the robot takeover... Do what ca can to save yourself and save the wor" +
+    "ld.";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.Gainsboro;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(9, 28);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(263, 201);
+            this.outputLabel.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(449, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(325, 242);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Robot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 296);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.optionButton3);
+            this.Controls.Add(this.optionButton2);
+            this.Controls.Add(this.optionButton1);
+            this.Name = "Robot";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button optionButton1;
+        private System.Windows.Forms.Button optionButton2;
+        private System.Windows.Forms.Button optionButton3;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,533 @@
+﻿using System;
+using System.Media;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Robot_Adventure_Final
+{
+    public partial class Robot : Form
+    {
+        // Tracks the page of the story the user is on
+        int page = 1;
+        SoundPlayer applause = new SoundPlayer(Properties.Resources.Applause);
+        SoundPlayer press = new SoundPlayer(Properties.Resources.PressSound);
+
+        //Random number generator
+        Random randGen = new Random();
+        public Robot()
+        {
+            InitializeComponent();
+
+            optionButton1.Visible = false;
+            optionButton2.Visible = false;
+            optionButton3.Visible = false;
+            outputLabel.Visible = false;
+            pictureBox1.Image = (Properties.Resources.Apocolypse);
+            applause = new SoundPlayer(Properties.Resources.Applause);
+            SoundPlayer press = new SoundPlayer(Properties.Resources.PressSound);
+
+
+        }
+
+        private void optionButton1_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 3;
+            }
+            else if (page == 2)
+            {
+                page = 5;
+            }
+            else if (page == 3)
+            {
+                page = 4;
+            }
+            else if (page == 4) //Fix robot with a 10% of chance. 
+            {
+                int randValue = randGen.Next(1, 101);
+
+                if (randValue <= 10)
+                {
+                    page = 11;
+                }
+                else
+                {
+                    page = 12;
+                }
+            }
+            else if (page == 5)
+
+            {
+                page = 1;
+            }
+            else if (page == 6)
+            {
+                page = 1;
+            }
+
+            else if (page == 7)
+            {
+                page = 8;
+            }
+            else if (page == 8)
+            {
+                page = 19;
+            }
+            else if (page == 9)
+            {
+                page = 1;
+            }
+            else if (page == 11)
+            {
+                page = 14;
+            }
+            else if (page == 12)
+            {
+                page = 1;
+            }
+
+            else if (page == 13)
+            {
+                page = 1;
+            }
+            else if (page == 14)
+            {
+                page = 1;
+            }
+            else if (page == 16)
+            {
+                page = 1;
+            }
+            else if (page == 17)
+            {
+                page = 1;
+            }
+            else if (page == 18)
+            {
+                page = 1;
+            }
+            else if (page == 19)
+            {
+                page = 1;
+            }
+            else if (page == 21)
+            {
+                page = 1;
+            }
+
+
+
+
+
+            DisplayPage();
+        }
+
+        private void optionButton3_Click(object sender, EventArgs e)
+        {
+            if (page == 11)
+            {
+                page = 13;
+
+            }
+            else if (page == 7)
+            {
+                page = 9;
+            }
+
+            DisplayPage();
+        }
+
+        private void optionButton2_Click(object sender, EventArgs e)
+        {
+
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2)
+            {
+                page = 6;
+            }
+            else if (page == 3)
+            {
+                page = 7;
+            }
+            else if (page == 4)
+            {
+                page = 18;
+            }
+            else if (page == 5)
+            {
+                page = 1000;
+            }
+            else if (page == 6)
+            {
+                page = 1000;
+            }
+            else if (page == 7)
+            {
+                page = 17;
+            }
+            else if (page == 8)
+            {
+                page = 21;
+            }
+            else if (page == 9)
+            {
+                page = 16;
+            }
+            else if (page == 11)
+            {
+                page = 12;
+            }
+
+            else if (page == 12)
+            {
+                page = 1000;
+            }
+
+            else if (page == 13)
+            {
+                page = 1000;
+            }
+            else if (page == 14)
+            {
+                page = 1000;
+            }
+            else if (page == 16)
+            {
+                page = 1000;
+            }
+            else if (page == 17)
+            {
+                page = 1000;
+            }
+            else if (page == 18)
+            {
+                page = 1000;
+            }
+            else if (page == 19)
+            {
+                page = 1000;
+            }
+            else if (page == 21)
+            {
+                page = 1000;
+            }
+
+            DisplayPage();
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            page = 1;
+            outputLabel.Visible = true;
+
+            DisplayPage();
+        }
+
+        private void DisplayPage()
+        {
+            switch (page)
+            {
+                case 1: //case 1 == page 1 and same for other cases
+
+                    optionButton1.Visible = true; //the button will be visible for player to select it
+                    optionButton2.Visible = true;
+                    outputLabel.Text = "You wakeup after the robot TAKEOVER. The world is left in ruins and you are the only form of human life left. Grab your weapons and explore the ruins...";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    startButton.Visible = false;
+                    titleLabel.Visible = false;
+                    optionButton3.Visible = false;
+                    pictureBox1.Image = (Properties.Resources.Wake);
+                    press.Play();
+
+                    break;
+                case 2:
+                    optionButton2.Visible = true;
+                    optionButton1.Visible = true;
+                    outputLabel.Text = "You stay quiet in your hidden room and discover news articles and clues to help you. Do you chose to begin exploring...";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    startButton.Visible = false;
+                    titleLabel.Visible = false;
+                    optionButton3.Visible = false; 
+                    press.Play();
+                    pictureBox1.Image = (Properties.Resources.SightWorld2);
+
+                    break;
+                case 3:
+                    optionButton2.Visible = true;
+                    optionButton1.Visible = true;
+                    outputLabel.Text = "You encounter a robot in the pile of ruins. He seems dead and broken. Do you chose to grab the robot for testing or leave it...";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    startButton.Visible = false;
+                    titleLabel.Visible = false;
+                    optionButton3.Visible = false;
+                    press.Play();
+                    pictureBox1.Image = (Properties.Resources.Friend);
+
+                    break;
+                case 4:
+                    optionButton2.Visible = true;
+                    optionButton1.Visible = true;
+                    outputLabel.Text += "you collect the robot, you bring it back home to examine further.";
+                    this.Refresh();
+                    Thread.Sleep(4000);
+                    outputLabel.Text += "You manage to fix the robot and have a chance to have a companion. Do you repair the robot hoping for a companion?";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    startButton.Visible = false;
+                    titleLabel.Visible = false;
+                    optionButton3.Visible = false;
+                    press.Play();
+                    pictureBox1.Image = (Properties.Resources.SightWorld3);
+
+                    break;
+                case 5:
+                    optionButton2.Visible = true;
+                    optionButton1.Visible = true;
+                    outputLabel.Text = "You find a female in the distance shot by the arrow of the robot army you cry and decide to give up. Play again...";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    startButton.Visible = false;
+                    titleLabel.Visible = false;
+                    optionButton3.Visible = false;
+                    press.Play();
+                    pictureBox1.Image = (Properties.Resources.SightWorld);
+
+                    break;
+                case 6:
+                    optionButton2.Visible = true;
+                    optionButton1.Visible = true;
+                    outputLabel.Text = "A robot army searching for human remains find you and terminate you. Play again...";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    startButton.Visible = false;
+                    titleLabel.Visible = false;
+                    optionButton3.Visible = false;
+                    press.Play();
+                    pictureBox1.Image = (Properties.Resources.Army);
+
+
+                    break;
+                case 7:
+                    optionButton2.Visible = true;
+                    optionButton1.Visible = true;
+                    optionButton3.Visible = true;
+                    outputLabel.Text = "You leave the robot and get attacked by firing drones. What do you do to evade their strikes...";
+                    optionButton1.Text = "Charge";
+                    optionButton2.Text = "Weapon";
+                    optionButton3.Text = "Hide";
+                    startButton.Visible = false;
+                    titleLabel.Visible = false;
+                    press.Play();
+                    pictureBox1.Image = (Properties.Resources.TurnBack);
+
+                    break;
+                case 8:
+                    optionButton2.Visible = true;
+                    optionButton1.Visible = true;
+                    outputLabel.Text = "You kill one and enter its suit. You invade the palace they built for themselves";
+                    this.Refresh();
+                    Thread.Sleep(4000);
+                    outputLabel.Text = "While exploring you come face to face with the robot leader and decide to strike. What do you go for...";
+                    optionButton1.Text = "Head";
+                    optionButton2.Text = "Face";
+                    startButton.Visible = false;
+                    optionButton3.Visible = false;
+                    titleLabel.Visible = false;
+                    press.Play();
+                    pictureBox1.Image = (Properties.Resources.Army);
+
+                    break;
+                case 9:
+                    optionButton2.Visible = true;
+                    optionButton1.Visible = true;
+                    outputLabel.Text = "You hide and your foot ends up being exposed and snapped off. Play again...";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    startButton.Visible = false;
+                    titleLabel.Visible = false;
+                    optionButton3.Visible = false;
+                    press.Play();
+                    pictureBox1.Image = (Properties.Resources.SightWorld2);
+
+
+                    break;
+                case 11:
+                    optionButton2.Visible = true;
+                    optionButton1.Visible = true;
+                    optionButton3.Visible = true;
+                    outputLabel.Text = "You fixed the robot, meet your deadly companion";
+                    this.Refresh();
+                    Thread.Sleep(4000);
+                    outputLabel.Text = "You and your robot companion set out on a final mission to take back the world. How do you plan on killing the robots...";
+                    optionButton1.Text = "Battlefield";
+                    optionButton2.Text = "Base";
+                    optionButton3.Text = "Leader";
+                    startButton.Visible = false;
+                    titleLabel.Visible = false;
+                    press.Play();
+                    pictureBox1.Image = (Properties.Resources.SightWorld3);
+
+
+
+
+                    break;
+                case 12:
+                    optionButton2.Visible = true;
+                    optionButton1.Visible = true;
+                    outputLabel.Text = "Your attempt failed and the robot recovered life and killed you. Play again...";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    startButton.Visible = false;
+                    titleLabel.Visible = false;
+                    optionButton3.Visible = false;
+                    press.Play();
+                    pictureBox1.Image = (Properties.Resources.SightWorld2);
+
+
+                    break;
+                case 13:
+                    optionButton2.Visible = true;
+                    optionButton1.Visible = true;
+                    outputLabel.Text = "You kill the leader and the army fails because your companion was a nuclear explosive";
+                    this.Refresh();
+                    Thread.Sleep(4000);
+                    outputLabel.Text = "You have completed the game friend. Play again...";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    startButton.Visible = false;
+                    titleLabel.Visible = false;
+                    optionButton3.Visible = false;
+                    applause.Play();
+                    pictureBox1.Image = (Properties.Resources.SightWorld3);
+                    //Win sound
+
+
+                    break;
+                case 14:
+                    optionButton2.Visible = true;
+                    optionButton1.Visible = true;
+                    outputLabel.Text = "You were immediately shot by a firing squad. Play again...";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    startButton.Visible = false;
+                    titleLabel.Visible = false;
+                    optionButton3.Visible = false;
+                    press.Play();
+                    pictureBox1.Image = (Properties.Resources.Army);
+
+                    break;
+                case 16:
+                    optionButton2.Visible = true;
+                    optionButton1.Visible = true;
+                    outputLabel.Text = "You hide and your foot ends up being exposed and snapped off. Play again...";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    startButton.Visible = false;
+                    titleLabel.Visible = false;
+                    optionButton3.Visible = false;
+                    press.Play();
+                    pictureBox1.Image = (Properties.Resources.TurnBack);
+
+
+
+                    break;
+                case 17:
+                    optionButton2.Visible = true;
+                    optionButton1.Visible = true;
+                    outputLabel.Text = "You find a sniper and begin shooting. You run out of ammo because you didnt check the mag. Play again...";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    startButton.Visible = false;
+                    titleLabel.Visible = false;
+                    optionButton3.Visible = false;
+                    press.Play();
+                    pictureBox1.Image = (Properties.Resources.TurnBack);
+
+                    break;
+                case 18:
+                    optionButton2.Visible = true;
+                    optionButton1.Visible = true;
+                    outputLabel.Text = "The robot comes alive and detonates you after detecting human body radiation. Play again...";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    startButton.Visible = false;
+                    titleLabel.Visible = false;
+                    optionButton3.Visible = false;
+                    press.Play();
+                    pictureBox1.Image = (Properties.Resources.GoOut);
+
+                    break;
+                case 19:
+                    optionButton2.Visible = true;
+                    optionButton1.Visible = true;
+                    outputLabel.Text = "You go for the head and unlike Thanos he falls down and mumbles This is not the end";
+                    this.Refresh();
+                    Thread.Sleep(4000);
+                    outputLabel.Text = "You have completed the game friend. Play again...";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    startButton.Visible = false;
+                    titleLabel.Visible = false;
+                    optionButton3.Visible = false;
+                    applause.Play();
+                    pictureBox1.Image = (Properties.Resources.GoOut);
+                    //Win sound
+
+
+                    break;
+
+                case 21:
+                    optionButton2.Visible = true;
+                    optionButton1.Visible = true;
+                    outputLabel.Text = "He moves out of the way laughing at you, he picks you up and sends you into orbit. Play again...";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    startButton.Visible = false;
+                    titleLabel.Visible = false;
+                    optionButton3.Visible = false;
+                    press.Play();
+                    pictureBox1.Image = (Properties.Resources.Army);
+
+
+                    break;
+
+
+                case 1000:
+                    optionButton2.Visible = false;
+                    optionButton1.Visible = false;
+                    outputLabel.Text = " You're bad at this game go home and play Fortnite...";
+                    optionButton1.Text = "Yes";
+                    optionButton2.Text = "No";
+                    startButton.Visible = false;
+                    titleLabel.Visible = false;
+                    optionButton3.Visible = false;
+                    press.Play();
+                    pictureBox1.Image = (Properties.Resources.TurnBack);
+                    Refresh();
+                    Thread.Sleep(4000);
+                    Application.Exit();
+
+
+                    break;
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
